fb6a2ac68fe210bb7cde2f7dd9c4027943c43b3e
rixwwd <rixwwd@users.noreply.github.com>
2016-07-24 20:12:01 +0900

    テストケース修正

--- test/controllers/certificates_controller_test.rb
+++ test/controllers/certificates_controller_test.rb
@@ -2,38 +2,39 @@ require 'test_helper'
 
 class CertificatesControllerTest < ActionDispatch::IntegrationTest
   setup do
+    @ca = cas(:one)
     @certificate = certificates(:one)
   end
 
   test "should get index" do
-    get certificates_url
+    get ca_certificates_url(@ca)
     assert_response :success
   end
 
   test "should get new" do
-    get new_certificate_url
+    get new_ca_certificate_url(@ca)
     assert_response :success
   end
 
   test "should create certificate" do
     assert_difference('Certificate.count') do
-      post certificates_url, params: { certificate: { ca: @certificate.ca, common_name: @certificate.common_name, country: @certificate.country, locality: @certificate.locality, not_after: @certificate.not_after, not_before: @certificate.not_before, organization_unit: @certificate.organization_unit, ornanization: @certificate.ornanization, state: @certificate.state
+      post ca_certificates_url(@ca), params: { certificate: { ca: @certificate.ca, common_name: @certificate.common_name, country: @certificate.country, locality: @certificate.locality, not_after: @certificate.not_after, not_before: @certificate.not_before, organization_unit: @certificate.organization_unit, organization: @certificate.organization, state: @certificate.state
          } }
     end
 
-    assert_redirected_to certificate_url(Certificate.last)
+    assert_redirected_to ca_certificate_url(@ca, Certificate.last)
   end
 
   test "should show certificate" do
-    get certificate_url(@certificate)
+    get ca_certificate_url(@ca, @certificate)
     assert_response :success
   end
 
   test "should destroy certificate" do
     assert_difference('Certificate.count', -1) do
-      delete certificate_url(@certificate)
+      delete ca_certificate_url(@ca, @certificate)
     end
 
-    assert_redirected_to certificates_url
+    assert_redirected_to ca_certificates_url(@ca)
   end
 end
